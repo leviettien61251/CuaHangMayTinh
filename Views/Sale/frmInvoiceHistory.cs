7bc453e8aaf0611bb383903b049836735c48abcd
Omitako <vakaoshsbsmdmx@gmail.com>
2025-09-08 08:13:04 +0700

    update file View

---
+++ Views/Sale/frmInvoiceHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuaHangMayTinh.Views.Sale
+{
+    public partial class frmInvoiceHistory: Form
+    {
+        public frmInvoiceHistory()
+        {
+            InitializeComponent();
+        }
+    }
+}
